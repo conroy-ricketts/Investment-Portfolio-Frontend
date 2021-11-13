54629c826512b868c547e864354744517d9f5416
conroyr41 <conroyr41@gmail.com>
2021-11-13 09:48:34 -0500

    set up main app

--- src/App.tsx
+++ src/App.tsx
@@ -11,3 +11,68 @@ export default function App() {
     </View>
   );
 }
+
+interface Transaction
+{
+  type: 'crypto' | 'stock',
+  orderType: 'buy' | 'sell' | 'transfer',
+  assetName: string,
+  amount: number, //shares if stock, amount if crypto
+  dateTime: string, //MM-DD-YYYY-HH-MM (millitary time)
+  price: number,
+  feeType: 'USD' | 'Native Asset',
+  fee: number,
+}
+
+export const testTransactionsAsJSON: Transaction[] = [
+  {
+    type: 'crypto',
+    orderType: 'buy',
+    assetName: 'BTC',
+    amount: 2.3,
+    dateTime: '01-26-2019-13-00',
+    price: 40000,
+    feeType: 'USD',
+    fee: 0.10,
+  },
+  {
+    type: 'crypto',
+    orderType: 'transfer',
+    assetName: 'BTC',
+    amount: 2.2,
+    dateTime: '02-13-2021-09-42',
+    price: 50000,
+    feeType: 'Native Asset',
+    fee: 0.1,
+  },
+  {
+    type: 'crypto',
+    orderType: 'sell',
+    assetName: 'BTC',
+    amount: 2.2,
+    dateTime: '11-13-2021-09-42',
+    price: 60000,
+    feeType: 'USD',
+    fee: 0.10,
+  },
+  {
+    type: 'stock',
+    orderType: 'buy',
+    assetName: 'MSFT',
+    amount: 1,
+    dateTime: '01-26-2019-13-00',
+    price: 200,
+    feeType: 'USD',
+    fee: 0,
+  },
+  {
+    type: 'stock',
+    orderType: 'sell',
+    assetName: 'MSFT',
+    amount: 0.5,
+    dateTime: '11-13-2021-09-42',
+    price: 150,
+    feeType: 'USD',
+    fee: 0,
+  },
+];
